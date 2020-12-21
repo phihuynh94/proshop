51d7d826bd1f50e51645aae885fefe2a54cbdb3e
phihuynh94 <phihuynh94@gmail.com>
2020-12-20 18:53:37 -0700

    setup redux state for homescreen & useDispatch, useSelector

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -8,6 +8,10 @@ import { listProducts } from "../actions/productActions";
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
+  const productList = useSelector((state) => state.productList);
+
+  const { loading, error, products } = productList;
+
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
@@ -15,13 +19,19 @@ const HomeScreen = () => {
   return (
     <>
       <h1>Lastest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
