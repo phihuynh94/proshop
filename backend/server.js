b8770efbaa8054825ce6f7964a623c6380d6ab3a
phihuynh94 <phihuynh94@gmail.com>
2020-12-22 11:04:09 -0700

    user authentication

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,21 @@ import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
