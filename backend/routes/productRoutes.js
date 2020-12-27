57e78a037f2fa5bbdaee2d127778bdf2476bd434
phihuynh94 <phihuynh94@gmail.com>
2020-12-27 14:40:06 -0700

    create & update product api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,13 +3,19 @@ import express from "express";
 const router = express.Router();
 
 import {
+  createProduct,
   deleteProduct,
   getProducts,
   getProductsById,
+  updateProduct,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductsById).delete(protect, admin, deleteProduct);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+  .route("/:id")
+  .get(getProductsById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
