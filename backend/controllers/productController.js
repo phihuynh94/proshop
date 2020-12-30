f3392026a6b6aa07425728d4ba7e7c3477195b4b
phihuynh94 <phihuynh94@gmail.com>
2020-12-30 14:46:46 -0700

    product search

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,7 +6,18 @@ import Product from "../models/productModel.js";
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  // Get keyword from the url query (url?keyword=<value>)
+  const keyword = req.query.keyword
+    ? {
+        // Match keyword without exact word
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const products = await Product.find({ ...keyword });
 
   res.json(products);
 });
