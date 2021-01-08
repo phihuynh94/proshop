1351899148648cffb64deddfa707f21da0b1819a
phihuynh94 <phihuynh94@gmail.com>
2021-01-08 14:41:48 -0700

    remove cart items when logout fix

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -64,6 +64,7 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_MY_RESET });
